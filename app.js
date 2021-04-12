983e5ae5703aa23ca46541586b1e642e936f2572
Megan <mtipps101@gmail.com>
2021-04-12 13:57:05 +0200

    Module: Express.js

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
-const http = require('http');
+const express = require('express');
 
-const routes = require('./routes');
+const app = express();
 
-const server = http.createServer(routes);
+app.use('/', (req, res, next) => {
+  console.log('This always runs!');
+  next();
+});
 
-server.listen(3000);
+app.use('/add-product', (req, res, next) => {
+  console.log('In another middleware!');
+  res.send('<h1>The "Add Product" page</h1>');
+});
+
+app.use('/', (req, res, next) => {
+  console.log('In another middleware!');
+  res.send('<h1>Hello from Express!</h1>');
+});
+
+app.listen(3000);
