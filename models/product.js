03a02d98369e50887e3f800003a717ee36bb59a8
Megan <mtipps101@gmail.com>
2021-05-05 14:12:53 +0200

    Module: Dynamic Routes & Advanced Models

--- models/product.js
+++ models/product.js
@@ -25,6 +25,7 @@ module.exports = class Product {
   }
 
   save() {
+    this.id = Math.random().toString();
     getProductsFromFile(products => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), (err) => {
@@ -36,4 +37,11 @@ module.exports = class Product {
   static fetchAll(cb) {
     getProductsFromFile(cb);
   }
-}
+
+  static findById(id, cb) {
+    getProductsFromFile(products => {
+      const product = products.find(product => product.id === id);
+      cb(product);
+    });
+  }
+};
