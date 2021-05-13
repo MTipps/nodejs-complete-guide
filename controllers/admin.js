b8104e825c30c0cb447a44cfb29e3cb304c36d04
Megan <mtipps101@gmail.com>
2021-05-13 10:00:37 +0200

    Module: Understanding Sequelize

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,18 +13,18 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  Product.create({
+  req.user.createProduct({
     title: title,
     price: price,
     imageUrl: imageUrl,
-    description: description
+    description: description,
   })
-      .then(result => {
-        // console.log(result)
-        console.log('Create Product')
-        res.redirect('/admin/products');
-      })
-      .catch(err => console.log(err))
+    .then(result => {
+      // console.log(result)
+      console.log('Create Product')
+      res.redirect('/admin/products');
+    })
+    .catch(err => console.log(err))
 };
 
 exports.getEditProduct = (req, res, next) => {
@@ -33,8 +33,9 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findByPk(prodId)
-      .then(product => {
+  req.user.getProducts({ where: { id: prodId } })
+      .then(products => {
+        const product = products[0];
         if (!product) {
           return res.redirect('/');
         }
@@ -70,7 +71,7 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
+  req.user.getProducts()
       .then(products => {
         res.render('admin/products', {
           prods: products,
